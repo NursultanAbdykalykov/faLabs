ef5820e17c50e87cbb6cc407341e9db907c9f5e5
Nursultan <znslt24@gmail.com>
2024-11-06 18:00:19 +0300

    fixed

--- 3LABS/task_3/task_3.c
+++ 3LABS/task_3/task_3.c
@@ -1,178 +1,224 @@
 #include <stdio.h>
-#include <stdlib.h>
 #include <string.h>
+#include <stdlib.h>
 
-typedef struct
-{
+enum StatusCodes {
+    ERROR = -1,
+    SUCCESS,
+};
+
+typedef int (*CallbackFunction)(const char* inputFile, const char* outputFile);
+
+typedef struct Employee {
+    float wage;
+    char lastName[BUFSIZ];
+    char firstName[BUFSIZ];
     unsigned int id;
-    char first_name[50];
-    char last_name[50];
-    double salary;
 } Employee;
 
-typedef enum
-{
-    SUCCESS = 0,
-    FILE_ERROR,
-    MEMORY_ERROR,
-} ErrorCode;
+int StringsEqual(const char* left, const char* right) {
+    return strcmp(left, right) == 0;
+}
 
-ErrorCode read_employees(const char *filename, Employee **employees, size_t *count)
-{
-    FILE *file = fopen(filename, "r");
-    if (!file)
-    {
-        return FILE_ERROR;
+int ValidateString(const char* str) {
+    for (int i = 0; str[i] != '\0'; ++i) {
+        if (!((str[i] >= 'a' && str[i] <= 'z') || (str[i] >= 'A' && str[i] <= 'Z'))) {
+            return 0;
+        }
+    }
+    return 1;
+}
+
+int ValidateWage(const char* str) {
+    int seenDecimalPoint = 0;
+    for (int i = 0; str[i] != '\0'; ++i) {
+        if (str[i] == '.') {
+            if (seenDecimalPoint) return 0;
+            seenDecimalPoint = 1;
+        } else if (str[i] < '0' || str[i] > '9') {
+            return 0;
+        }
+    }
+    return 1;
+}
+
+int ValidateId(const char* str) {
+    for (int i = 0; str[i] != '\0'; ++i) {
+        if (str[i] < '0' || str[i] > '9') {
+            return 0;
+        }
+    }
+    return 1;
+}
+
+int ValidateEmployeeData(const char* id, const char* firstName, const char* lastName, const char* wage) {
+    return ValidateId(id) && ValidateString(firstName) && ValidateString(lastName) && ValidateWage(wage);
+}
+
+int FindFlagIndex(const char* arg, const char* flags[], int numFlags) {
+    for (int i = 0; i < numFlags; ++i) {
+        if (StringsEqual(arg, flags[i])) return i;
+    }
+    return ERROR;
+}
+
+int CompareAscending(const void* a, const void* b) {
+    Employee* emp1 = (Employee*)a;
+    Employee* emp2 = (Employee*)b;
+    if (emp1->wage != emp2->wage) return (emp1->wage > emp2->wage) ? 1 : -1;
+    int firstNameComparison = strcmp(emp1->firstName, emp2->firstName);
+    if (firstNameComparison != 0) return firstNameComparison;
+    int lastNameComparison = strcmp(emp1->lastName, emp2->lastName);
+    return lastNameComparison != 0 ? lastNameComparison : (emp1->id > emp2->id ? 1 : -1);
+}
+
+int CompareDescending(const void* a, const void* b) {
+    Employee* emp1 = (Employee*)a;
+    Employee* emp2 = (Employee*)b;
+    if (emp1->wage != emp2->wage) return (emp1->wage < emp2->wage) ? 1 : -1;
+    int firstNameComparison = strcmp(emp1->firstName, emp2->firstName);
+    if (firstNameComparison != 0) return -firstNameComparison;
+    int lastNameComparison = strcmp(emp1->lastName, emp2->lastName);
+    return lastNameComparison != 0 ? -lastNameComparison : (emp1->id < emp2->id ? 1 : -1);
+}
+
+int ProcessFileAscending(const char* inputFile, const char* outputFile) {
+    FILE* in = fopen(inputFile, "r");
+    if (!in) return ERROR;
+
+    FILE* out = fopen(outputFile, "w");
+    if (!out) {
+        fclose(in);
+        return ERROR;
     }
 
-    size_t capacity = 10;
-    *employees = malloc(capacity * sizeof(Employee));
-    if (!*employees)
-    {
-        fclose(file);
-        return MEMORY_ERROR;
+    int index = 0, capacity = 10;
+    Employee* employees = malloc(capacity * sizeof(Employee));
+    if (!employees) {
+        fclose(in);
+        fclose(out);
+        return ERROR;
     }
 
-    *count = 0;
-    while (fscanf(file, "%u %49s %49s %lf", &(*employees)[*count].id,
-                  (*employees)[*count].first_name,
-                  (*employees)[*count].last_name,
-                  &(*employees)[*count].salary) == 4)
-    {
-        (*count)++;
-        if (*count >= capacity)
-        {
+    char id[BUFSIZ], firstName[BUFSIZ], lastName[BUFSIZ], wage[BUFSIZ];
+    while (fscanf(in, "%s %s %s %s", id, firstName, lastName, wage) != EOF) {
+        if (!ValidateEmployeeData(id, firstName, lastName, wage)) {
+            fclose(in);
+            fclose(out);
+            free(employees);
+            return ERROR;
+        }
+
+        if (index >= capacity) {
             capacity *= 2;
-            *employees = realloc(*employees, capacity * sizeof(Employee));
-            if (!*employees)
-            {
-                fclose(file);
-                return MEMORY_ERROR;
+            employees = realloc(employees, capacity * sizeof(Employee));
+            if (!employees) {
+                fclose(in);
+                fclose(out);
+                return ERROR;
             }
         }
+
+        employees[index].id = atoi(id);
+        strcpy(employees[index].firstName, firstName);
+        strcpy(employees[index].lastName, lastName);
+        employees[index].wage = atof(wage);
+        ++index;
     }
+    fclose(in);
 
-    fclose(file);
-    return SUCCESS;
-}
+    qsort(employees, index, sizeof(Employee), CompareAscending);
 
-ErrorCode write_employees(const char *filename, Employee *employees, size_t count)
-{
-    FILE *file = fopen(filename, "w");
-    if (!file)
-    {
-        return FILE_ERROR;
+    for (int i = 0; i < index; ++i) {
+        fprintf(out, "%u %s %s %f\n", employees[i].id, employees[i].firstName, employees[i].lastName, employees[i].wage);
     }
-
-    char buffer[200];
-
-    for (size_t i = 0; i < count; i++)
-    {
-        snprintf(buffer, sizeof(buffer), "%u %.49s %.49s %.2f\n", employees[i].id,
-                 employees[i].first_name,
-                 employees[i].last_name,
-                 employees[i].salary);
-        fputs(buffer, file);
-    }
-
-    fclose(file);
-    return SUCCESS;
-}
-
-int compare_employees(const void *a, const void *b)
-{
-    const Employee *emp1 = (const Employee *)a;
-    const Employee *emp2 = (const Employee *)b;
-
-    if (emp1->salary != emp2->salary)
-    {
-        return (emp1->salary < emp2->salary) ? -1 : 1;
-    }
-
-    int last_name_cmp = strcmp(emp1->last_name, emp2->last_name);
-    if (last_name_cmp != 0)
-    {
-        return last_name_cmp;
-    }
-
-    int first_name_cmp = strcmp(emp1->first_name, emp2->first_name);
-    if (first_name_cmp != 0)
-    {
-        return first_name_cmp;
-    }
-
-    return (emp1->id < emp2->id) ? -1 : (emp1->id > emp2->id);
-}
-
-int main(int argc, char *argv[])
-{
-    if (argc != 4)
-    {
-        fprintf(stderr, "Неверное количество аргументов\n");
-        return EXIT_FAILURE;
-    }
-
-    Employee *employees = NULL;
-    size_t count = 0;
-
-    ErrorCode result = read_employees(argv[1], &employees, &count);
-
-    if (result != SUCCESS)
-    {
-        if (result == FILE_ERROR)
-        {
-            fprintf(stderr, "Ошибка: не удалось открыть входной файл.\n");
-        }
-        else if (result == MEMORY_ERROR)
-        {
-            fprintf(stderr, "Ошибка: недостаточно памяти.\n");
-        }
-        return EXIT_FAILURE;
-    }
-
-    qsort(employees, count, sizeof(Employee), compare_employees);
-
-    if (strcmp(argv[2], "-d") == 0 || strcmp(argv[2], "d") == 0 ||
-        strcmp(argv[2], "/d") == 0 || strcmp(argv[2], "/d") == 0)
-    {
-        for (size_t i = 0; i < count / 2; i++)
-        {
-            Employee temp = employees[i];
-            employees[i] = employees[count - i - 1];
-            employees[count - i - 1] = temp;
-        }
-
-        result = write_employees(argv[3], employees, count);
-
-        if (result != SUCCESS)
-        {
-            fprintf(stderr, "Ошибка: не удалось открыть выходной файл.\n");
-            free(employees);
-            return EXIT_FAILURE;
-        }
-
-        free(employees);
-
-        return EXIT_SUCCESS;
-    }
-    else if (!(strcmp(argv[2], "-a") == 0 || strcmp(argv[2], "a") == 0 ||
-               strcmp(argv[2], "/a") == 0))
-    {
-        fprintf(stderr, "Ошибка: неверный флаг сортировки. Используйте -a или a для сортировки по возрастанию или -d или d для сортировки по убыванию.\n");
-        free(employees);
-        return EXIT_FAILURE;
-    }
-
-    result = write_employees(argv[3], employees, count);
-
-    if (result != SUCCESS)
-    {
-        fprintf(stderr, "Ошибка: не удалось открыть выходной файл.\n");
-        free(employees);
-        return EXIT_FAILURE;
-    }
-
+    fclose(out);
     free(employees);
 
-    return EXIT_SUCCESS;
+    return SUCCESS;
+}
+
+int ProcessFileDescending(const char* inputFile, const char* outputFile) {
+    FILE* in = fopen(inputFile, "r");
+    if (!in) return ERROR;
+
+    FILE* out = fopen(outputFile, "w");
+    if (!out) {
+        fclose(in);
+        return ERROR;
+    }
+
+    int index = 0, capacity = 10;
+    Employee* employees = malloc(capacity * sizeof(Employee));
+    if (!employees) {
+        fclose(in);
+        fclose(out);
+        return ERROR;
+    }
+
+    char id[BUFSIZ], firstName[BUFSIZ], lastName[BUFSIZ], wage[BUFSIZ];
+    while (fscanf(in, "%s %s %s %s", id, firstName, lastName, wage) != EOF) {
+        if (!ValidateEmployeeData(id, firstName, lastName, wage)) {
+            fclose(in);
+            fclose(out);
+            free(employees);
+            return ERROR;
+        }
+
+        if (index >= capacity) {
+            capacity *= 2;
+            employees = realloc(employees, capacity * sizeof(Employee));
+            if (!employees) {
+                fclose(in);
+                fclose(out);
+                return ERROR;
+            }
+        }
+
+        employees[index].id = atoi(id);
+        strcpy(employees[index].firstName, firstName);
+        strcpy(employees[index].lastName, lastName);
+        employees[index].wage = atof(wage);
+        ++index;
+    }
+    fclose(in);
+
+    qsort(employees, index, sizeof(Employee), CompareDescending);
+
+    for (int i = 0; i < index; ++i) {
+        fprintf(out, "%u %s %s %.2f\n", employees[i].id, employees[i].firstName, employees[i].lastName, employees[i].wage);
+    }
+    fclose(out);
+    free(employees);
+
+    return SUCCESS;
+}
+
+int main(int argc, char *argv[]) {
+    const char* flags[] = {"-a", "/a", "-d", "/d"};
+    CallbackFunction operations[] = {ProcessFileAscending, ProcessFileDescending};
+
+    if (argc != 4) {
+        fprintf(stderr, "USAGE: <input_file> <flag> <output_file>\n");
+        return ERROR;
+    }
+
+    int flagIndex = FindFlagIndex(argv[2], flags, sizeof(flags) / sizeof(char*));
+    if (flagIndex == ERROR) {
+        fprintf(stdout, "ERROR: unknown flag\n");
+        return ERROR;
+    }
+
+    if (StringsEqual(argv[1], argv[3])) {
+        fprintf(stdout, "ERROR: input and output files must be different\n");
+        return ERROR;
+    }
+
+    int result = operations[flagIndex / 2](argv[1], argv[3]);
+    if (result == ERROR) {
+        fprintf(stdout, "ERROR: operation failed\n");
+        return ERROR;
+    }
+
+    return SUCCESS;
 }
